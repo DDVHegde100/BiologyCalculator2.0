fa2e9dc798c61726921a873adace7daf01b656bc
Dhruv Hegde <69629876+DDVHegde100@users.noreply.github.com>
2021-08-16 11:02:18 -0400

    Add files via upload

---
+++ main - 2021-08-16T105951.013.py
@@ -0,0 +1,12 @@
+print("Welcome to Dhruv's Biology Calculator")
+bio=str(input('Enter the calculation(no capital letters(single word)): '))
+
+if(bio=='ph'):
+    concentration=float(input('Enter the hydrogen concentration:'))
+    ph=-log(concentration)
+    print('%0.3f is the pH.' %(ph))
+if(bio=='BMI'):
+    Height=float(input("Enter your height in inches: "))
+    Weight=float(input("Enter your Weight in lbs: "))
+    BMI=(Weight/(Height*Height))*703
+    print("your Body Mass Index is: ",BMI)
